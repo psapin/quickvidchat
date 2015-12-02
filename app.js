c772d7dda8f31c8be75236159afa9526f47cace9
psapin <psapin@users.noreply.github.com>
2015-12-02 00:09:04 -0600

    EasyRTC lib multiparty demo + HD

---
+++ app.js
@@ -0,0 +1,93 @@
+
+
+// Load required modules
+var http    = require("http");              // http server core module
+var express = require("express");           // web framework external module
+var io      = require("socket.io");         // web socket external module
+var easyrtc = require("easyrtc");           // EasyRTC external module
+var path = require('path');
+
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var httpApp = express();
+var routes = require('./routes/index');
+httpApp.use(express.static(path.join(__dirname, 'public')));
+httpApp.use('/', routes);
+/*httpApp.configure(function() {
+    httpApp.use(express.static(__dirname + "/public/"));
+});*/
+// view engine setup
+httpApp.set('views', path.join(__dirname, 'views'));
+httpApp.set('view engine', 'jade');
+
+// Start Express http server on port 8080
+var webServer = http.createServer(httpApp).listen(8080);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer, {"log level":1});
+
+// Start EasyRTC server
+var rtc = easyrtc.listen(httpApp, socketServer);
+
+
+
+/*var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
+*/
